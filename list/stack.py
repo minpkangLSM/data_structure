51f7da7bcf8cd8c70b004849b12244bb74072a9a
minpkang <minpkang@gmail.com>
2021-09-28 18:03:04 +0900

    stack

---
+++ list/stack.py
@@ -0,0 +1,66 @@
+# Simple version : Python list
+def push(stack, item):
+    stack.append(item)
+    return stack
+
+def peek(stack):
+    if len(stack)!=0:
+        return stack[-1]
+
+def pop(stack):
+    if len(stack)!=0:
+        item = stack.pop()
+        return item
+
+# stack = []
+# stack = push(stack, "apple")
+# stack = push(stack, "orange")
+# stack = push(stack, "cherry")
+# pe = peek(stack)
+# po = pop(stack)
+# print(po)
+# pe = peek(stack)
+# print(pe)
+
+# Singlylinkedlist
+class Node :
+    def __init__(self, item, next):
+        self.item = item
+        self.next = next
+
+def push(item):
+    global top
+    global size
+    top = Node(item, top)
+    size += 1
+
+def peek():
+    if size != 0:
+        return top.item
+
+def pop():
+    global top
+    global size
+    if size!=0:
+        top_item = top.item
+        top = top.next
+        size -= 1
+        return top_item
+
+def print_stack():
+    p = top
+    while p:
+        if p.next!=None:
+            print(p.item,"->",end="")
+        else:
+            print(p.item)
+        p = p.next
+
+top = None
+size = 0
+push("apple")
+push("oragne")
+push("cherry")
+print_stack()
+pop()
+print_stack()
