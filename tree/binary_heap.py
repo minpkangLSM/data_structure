536d17cd340b09ebf02de931253ac4d7cf8a1544
minpkang <minpkang@gmail.com>
2021-09-29 17:43:25 +0900

    refactor

--- tree/binary_heap.py
+++ tree/binary_heap.py
@@ -1,51 +1,50 @@
 # binary heap : 21.09.29.
 class binary_heap :
-
     def __init__(self, array):
         self.array = array
         self.num_elem = len(array)-1
 
+    def downheap(self, i):
+        while 2*i<=self.num_elem:
+            child=2*i
+            if child<self.num_elem and self.array[child] > self.array[child+1]:
+                child+=1
+            if self.array[i] < self.array[child]:
+                break
+            temp = self.array[i]
+            self.array[i] = self.array[child]
+            self.array[child] = temp
+            i = child
+
+    def upheap(self, j):
+        while j>1 and self.array[j] < self.array[j//2]:
+            temp = self.array[j//2]
+            self.array[j//2] = self.array[j]
+            self.array[j] = temp
+            j = j//2
+
     def create_heap(self):
-        for i in range(self.num_elem//2, 0, -1):
-            self.downheap(i)
+        for idx in range(self.num_elem//2, 0, -1):
+            self.downheap(idx)
+
+    def insert(self, key_value):
+        self.array.append(key_value)
+        self.num_elem += 1
+        self.upheap(self.num_elem)
 
     def delete_min(self):
-        if self.num_elem==0:
+        if self.num_elem == 0:
             print("EMPTY.")
             return
-        minimum = self.array[1]
+        min = self.array[1]
         self.array[1] = self.array[-1]
         del self.array[-1]
         self.num_elem -= 1
         self.downheap(1)
-        return minimum
-
-    def downheap(self, i):
-        while 2*i <= self.num_elem:
-            child=2*i
-            if child < self.num_elem and self.array[child] > self.array[child+1]:
-                child+=1
-            if self.array[child] > self.array[i]:
-                break
-            temp = self.array[child]
-            self.array[child] = self.array[i]
-            self.array[i] = temp
-            i = child
-
-    def insert(self, value):
-        self.num_elem += 1
-        self.array.append(value)
-        self.uphead(self.num_elem)
-
-    def uphead(self, j):
-        while j > 1 and self.array[j] < self.array[j//2]:
-            temp = self.array[j]
-            self.array[j] = self.array[j//2]
-            self.array[j//2] = temp
-            j = j//2
+        return min
 
     def print_heap(self):
-        for i in range(1, self.num_elem):
+        for i in range(1, self.num_elem+1):
             print(self.array[i])
         print("SIZE : ", self.num_elem)
 
